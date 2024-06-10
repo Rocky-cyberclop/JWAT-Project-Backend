7e4123c4b59e5fac1fe9f338647389aea397cada
Rocky-cyberclop <rockyoperation@gmail.com>
2024-06-10 11:51:16 +0700

    [Project CRUD] Complete project CRUD

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -2,33 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { Project } from './entities/project.entity';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
+// @Roles(Role.MANAGER)
 @Controller('project')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
+  create(@Body() createProjectDto: CreateProjectDto): Promise<Project> {
     return this.projectService.create(createProjectDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Project[]> {
     return this.projectService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Project> {
     return this.projectService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProjectDto: UpdateProjectDto,
+  ): Promise<UpdateResult> {
     return this.projectService.update(+id, updateProjectDto);
   }
 
+  // @Roles(Role.ADMIN)
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<DeleteResult> {
     return this.projectService.remove(+id);
   }
 }
